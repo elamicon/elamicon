fa03b58dae7d61bc81c74ba4fcd00e5f70275598
Stephan Balmer <sb@cis.ch>
2019-07-06 23:42:42 +0200

    doc

--- src/ScriptDefs.elm
+++ src/ScriptDefs.elm
@@ -16,10 +16,24 @@ type alias SyllabaryDef =
     { id : String, name : String, syllabary : String }
 
 
+-- Details about a group
+--   short: ID of the group
+--   name: Descriptive name for the group
+--   recorded: whether there is an archaelogical paper trail fro
+--             fragments in this group.
 type alias GroupDef =
     { short : String, name : String, recorded : Bool }
 
 
+-- Fragement of the text body
+--  id: catalogue ID, unique within the corpus.
+--  group: group ID.
+--  dir: Writing direction. This may be a guess.
+--  text: Text body. The character order is always in the expected
+--        read order. So an inscription that should look like "123" when
+--        displayed but is read from the right must be written "321" in the
+--        corpus.
+--  plate: Optional name of a file with details
 type alias FragmentDef =
     { id : String, group : String, dir : Dir, text : String, plate : Maybe String }
 
