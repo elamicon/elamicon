cb290c4017f8486a68545806548e873158d28cd6
Stephan Balmer <sb@cis.ch>
2020-03-09 21:53:30 +0100

    Raetic search examples

--- src/Raetic.elm
+++ src/Raetic.elm
@@ -24,11 +24,11 @@ indexedTokens = Set.fromList (wildcardChar :: tokens)
 indexed char = Set.member char indexedTokens
 
 searchExamples =
-    [ ("[]", "Search occurrences of  followed by either  or ")
-    , ("(.)\\1", "Look for sign repetitions (geminates) like ")
+    [ ("[]", "Search occurrences of  followed by either  or ")
+    , ("(.)\\1", "Look for sign repetitions (geminates) like ")
     , ("([^])\\1", "Look for sign repetitions (geminates) excluding placeholder ")
-    , ("(.).\\1", "Sign repetitions with an arbitrary sign in-between ()")
-    , ("[]", "Show all occurrences of  and ")
+    , ("(.).\\1", "Sign repetitions with an arbitrary sign in-between ()")
+    , ("[]", "Show all occurrences of  and ")
     ]
 
 -- We don't know enough about the language to venture guesses about
@@ -78,13 +78,14 @@ Raetic denominates, as in TIR, all North Italic inscriptions that are neither wr
 #### Terminology used for labelling the sign variants
 
 **Writing direction:** sin = sinistroverse; dex = dextroverse; sin/dex = used in this exact shape (i.e. without mirroring the sign) in both writing directions.
+
 **Sub-numeration of the signs:** For Raetic and Lepontic, the sub-numeration is taken from the respective main sources. Supplementary sign variants added by us and not present in the main source have been labelled with further sub-numbers.
 We have labelled the sign variants with sub-numbers according to the frequency of occurrence, i.e.: Raet A₁ is the most frequent Raetic sign variant with the sound value A, Raet A₂ is the second most frequent Raetic sign variant with the sound value A etc. (There are exceptions from this basic rule, due to the iterative compilation of the sub-corpora.)
 
 
 #### Sigla
 
-Inscription sigla (such as RN-1) are taken from, and kept in sync with, TIR. In
+Inscription sigla (such as RN-1) are taken from and kept in sync with TIR. In
 addition, the inscriptions are grouped into what TIR terms "alphabets". They are
 shown as superscript to the sigla, example: <sup>MAG</sup>AK-1.1. The superscripts
 used are:
